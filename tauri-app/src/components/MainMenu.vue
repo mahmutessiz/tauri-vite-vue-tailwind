b0af99565b9de3a1fc60d91db1174111f80cf987
mahmutessiz <mahmutessiz71@gmail.com>
2023-10-04 20:18:28 +0300

    first commit

---
+++ tauri-app/src/components/MainMenu.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="w-full grid place-items-center p-4 bg-red-800">
+        <ul class="flex gap-4">
+            <li class=" text-xl hover:text-blue-400 cursor-pointer">Home</li>
+            <li class=" text-xl hover:text-blue-400 cursor-pointer">About</li>
+            <li class=" text-xl hover:text-blue-400 cursor-pointer">Deneme</li>
+        </ul>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
